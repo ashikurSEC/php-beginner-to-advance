9aedf62ea9f2032b62a3dfe9cec0011e689b6061
ashikurSEC <ashikurrahman7194@gmail.com>
2024-04-04 23:28:27 +0600

    My first php example

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php 
-
-echo "Hello World";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Message</title>
+</head>
+<body>
+    <h1>
+        <?php echo "Hello world" ?>
+    </h1>
+</body>
+</html>
